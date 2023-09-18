fde6d20e2b94b19b34e33df9b1560867a82448f6
Administrador <jesusemanuelgarcia@gmail.com>
2023-09-18 18:59:32 -0300

    v01-inicio

---
+++ app/src/main/java/com/example/tp07_login_menu_geolocalizacion_musica/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp07_login_menu_geolocalizacion_musica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TP07_Login-Menu-Geolocalizacion-Musica"
+include(":app")
