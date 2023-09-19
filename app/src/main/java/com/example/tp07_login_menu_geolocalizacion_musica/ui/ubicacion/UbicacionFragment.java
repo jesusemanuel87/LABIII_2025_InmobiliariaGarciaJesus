5ffb19c300bcfaf063b79b241f94062b2f8791f0
jesusemanuel87 <jesusemanuelgarcia@gmail.com>
2023-09-19 02:10:37 -0300

    Add location arrangement

--- app/src/main/java/com/example/tp07_login_menu_geolocalizacion_musica/ui/ubicacion/UbicacionFragment.java
+++ app/src/main/java/com/example/tp07_login_menu_geolocalizacion_musica/ui/ubicacion/UbicacionFragment.java
@@ -1,6 +1,11 @@
 package com.example.tp07_login_menu_geolocalizacion_musica.ui.ubicacion;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +17,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.tp07_login_menu_geolocalizacion_musica.R;
 import com.example.tp07_login_menu_geolocalizacion_musica.databinding.FragmentUbicacionBinding;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class UbicacionFragment extends Fragment {
-
     private FragmentUbicacionBinding binding;
-
     private UbicacionViewModel mv;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,14 +30,27 @@ public class UbicacionFragment extends Fragment {
         mv = new ViewModelProvider(this).get(UbicacionViewModel.class);
         binding = FragmentUbicacionBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        solicitarPermisos();
 
-        mv.getMLocation().observe(getViewLifecycleOwner(), new Observer<Location>() {
+        mv.getMMapa().observe(getViewLifecycleOwner(), new Observer<UbicacionViewModel.MapaActual>() {
             @Override
-            public void onChanged(Location location) {
-                binding.tvUbicacion.setText(location.getLatitude()+" "+location.getLongitude());
+            public void onChanged(UbicacionViewModel.MapaActual mapaActual) {
+                SupportMapFragment smf=(SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+                smf.getMapAsync(mapaActual);
             }
         });
+        //mv.obtenerUltimaUbicacion();
+        //mv.lecturaPermanente();
+        mv.obtenerMapa();
         return root;
+    }
+
+    public void solicitarPermisos(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && (requireContext().checkSelfPermission(ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
+                (requireContext().checkSelfPermission(ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            requestPermissions(new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, 1000);
+        }
     }
 
     @Override
--- app/src/main/java/com/example/tp07_login_menu_geolocalizacion_musica/ui/ubicacion/UbicacionViewModel.java
+++ app/src/main/java/com/example/tp07_login_menu_geolocalizacion_musica/ui/ubicacion/UbicacionViewModel.java
@@ -6,6 +6,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.AndroidViewModel;
@@ -14,42 +17,107 @@ import androidx.lifecycle.MutableLiveData;
 
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class UbicacionViewModel extends AndroidViewModel {
 
-    Context context;
-    FusedLocationProviderClient fused;
-    MutableLiveData<Location> mLocation;
+    private Context context;
+    private FusedLocationProviderClient fused;
+    private MutableLiveData<Location> mLocation;
+    private MutableLiveData<MapaActual> mMapa;
 
     public UbicacionViewModel(@NonNull Application application) {
         super(application);
-        context = application.getApplicationContext();
+        this.context = application;
         fused = LocationServices.getFusedLocationProviderClient(context);
     }
-    public LiveData<Location> getMLocation(){
-        if(mLocation == null){
+    public LiveData<Location> getMLocation() {
+        if (mLocation == null) {
             mLocation = new MutableLiveData<>();
         }
         return mLocation;
     }
-    public void obtenerUltimaUbicacion() {
+    public LiveData<MapaActual> getMMapa(){
+        if (mMapa==null){
+            mMapa=new MutableLiveData<>();
+        }
+        return mMapa;
+    }
+//    public void obtenerUltimaUbicacion() {
+//        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+//                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            return;
+//        }
+//        Task<Location> tarea = fused.getLastLocation();
+//        tarea.addOnSuccessListener(getApplication().getMainExecutor(), new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                if (location != null) {
+//                    mLocation.postValue(location);
+//                }
+//            }
+//        });
+//    }
+
+    public void lecturaPermanente() {
+        LocationRequest request = LocationRequest.create();
+        request.setInterval(5000);
+        request.setFastestInterval(5000);
+        request.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+
+        LocationCallback callback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    mLocation.postValue(location);
+                }
+            }
+        };
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-
+            //    ActivityCompat#requestPermissions.
             return;
         }
-        Task<Location> tarea = fused.getLastLocation();
-        tarea.addOnSuccessListener(getApplication().getMainExecutor(), new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
+        fused.requestLocationUpdates(request, callback, null);
+    }
 
-                mLocation.postValue(location);
+    public void obtenerMapa(){
+        MapaActual mapaActual=new MapaActual();
+        mMapa.setValue(mapaActual);
+    }
+    public class MapaActual implements OnMapReadyCallback{
+        @Override
+        public void onMapReady(@NonNull GoogleMap googleMap) {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+            Location location= mLocation.getValue();
+            if (location!=null) {
+                LatLng latLng=new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("Mi ubicación"));
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latLng)
+                        .zoom(20)
+                        .bearing(45)
+                        .tilt(70)
+                        .build();
+                CameraUpdate update = CameraUpdateFactory.newCameraPosition(cameraPosition);
+                googleMap.animateCamera(update);
             }
-        });
+        }
     }
 }
